3c925814679021ae050e6d8fec52d1c1fc6b61da
cjy <cjy632258@hotmail.com>
2018-03-20 18:44:16 +0800

    first

---
+++ src/renderer/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'evue-demo'
+  }
+</script>
+
+<style>
+  /* CSS */
+  @import "~vuetify/dist/vuetify.css";
+  @import "app.css";
+</style>
---
+++ src/renderer/pages/Index.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app id="app-main">
+      <v-toolbar flat dark style="-webkit-app-region: drag;opacity:.6;" height="32">
+          <v-btn fab color="red" flat @click="closeWin" style="width:28px;height:28px">x</v-btn>
+      </v-toolbar>
+      <v-container app>
+        <v-card ripple>
+            <v-card-title>{{ info.cpu }}</v-card-title>
+        </v-card>
+      </v-container>
+  </v-app>
+</template>
+<script>
+import {ipcRenderer} from 'electron';
+export default {
+    data(){
+        return {
+            info:{
+                cpu: process.memoryUsage()
+            }
+        }
+    },
+    mounted(){
+    },
+    methods:{
+        closeWin(){
+            ipcRenderer.send('close')
+        }
+    },
+
+}
+</script>
